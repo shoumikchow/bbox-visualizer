e8e50a61020ceaafeba11aed0a7a9db7967c171d
Shoumik <shoumikchow@gmail.com>
2019-06-28 15:04:37 +0600

    Added a new labeling method and modified draw_rectangle - Added T label that looks like a T, originating from middle of top edge of rectangle - Combined drawing solid rectangle and opaque rectangle together

--- bbox_visualizer.py
+++ bbox_visualizer.py
@@ -1,7 +1,12 @@
 import cv2
 
 
-def draw_rectangle(img, bbox, bbox_color=(255, 255, 255), thickness=3, is_opaque=False, alpha=0.5):
+def draw_rectangle(img,
+                   bbox,
+                   bbox_color=(255, 255, 255),
+                   thickness=3,
+                   is_opaque=False,
+                   alpha=0.5):
     """Draws the rectangle around the object
     
     Parameters
@@ -15,26 +20,28 @@ def draw_rectangle(img, bbox, bbox_color=(255, 255, 255), thickness=3, is_opaque
     thickness : int, optional
         thickness of the outline of the box, by default 3
     is_opaque : bool, optional
-        determines if the rectangle should be semi-transparent or not, by default False
+        if False, draws a solid rectangular outline. Else, a filled rectangle which is semi transparent, by default False
     alpha : float, optional
-        strenght of the opacity, by default 0.5
-    
+        strength of the opacity, by default 0.5
     Returns
     -------
     ndarray
         the image with the bounding box drawn
     """
+
     output = img.copy()
     if not is_opaque:
-        cv2.rectangle(output, (bbox[0], bbox[1]), (bbox[2], bbox[3]), bbox_color,
-                  thickness)
+        cv2.rectangle(output, (bbox[0], bbox[1]), (bbox[2], bbox[3]),
+                      bbox_color, thickness)
     else:
         overlay = img.copy()
-        cv2.rectangle(overlay, (bbox[0], bbox[1]), (bbox[2], bbox[3]), bbox_color,
-                  -1)
+
+        cv2.rectangle(overlay, (bbox[0], bbox[1]), (bbox[2], bbox[3]),
+                      bbox_color, -1)
         cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
 
     return output
+
 
 def add_label_to_rectangle(img,
                            label,
@@ -68,34 +75,61 @@ def add_label_to_rectangle(img,
         the image with the label written
     """
 
-    text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+    text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0][0]
 
     if top:
-        label_bg = [bbox[0], bbox[1], bbox[0] + text_width[0], bbox[1] - 30]
+        label_bg = [bbox[0], bbox[1], bbox[0] + text_width, bbox[1] - 30]
         if draw_bg:
             cv2.rectangle(img, (label_bg[0], label_bg[1]),
-                        (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
+                          (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
         cv2.putText(img, label, (bbox[0] + 5, bbox[1] - 5),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
 
     else:
-        label_bg = [bbox[0], bbox[1], bbox[0] + text_width[0], bbox[1] + 30]
+        label_bg = [bbox[0], bbox[1], bbox[0] + text_width, bbox[1] + 30]
         if draw_bg:
             cv2.rectangle(img, (label_bg[0], label_bg[1]),
-                        (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
+                          (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
         cv2.putText(img, label, (bbox[0] + 5, bbox[1] - 5 + 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
 
     return img
 
 
+def add_T_label_to_rectangle(img,
+                             label,
+                             bbox,
+                             draw_bg=True,
+                             text_bg_color=(255, 255, 255),
+                             text_color=(0, 0, 0)):
+
+    text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0][0]
+    text_height = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0][1]
+
+    # draw vertical line
+    x_center = (bbox[0] + bbox[2]) // 2
+    y_top = bbox[1] - 50
+    cv2.line(img, (x_center, bbox[1]), (x_center, y_top), text_bg_color, 3)
+
+    # draw rectangle with label
+    y_bottom = y_top
+    y_top = y_bottom - text_height - 5
+    x_left = x_center - (text_width // 2) - 5
+    x_right = x_center + (text_width // 2) + 5
+    if draw_bg:
+        cv2.rectangle(img, (x_left, y_top - 3), (x_right, y_bottom),
+                      text_bg_color, -1)
+    cv2.putText(img, label, (x_left + 5, y_bottom - 7),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
+
+
 def draw_flag_with_label(img,
-                      label,
-                      bbox,
-                      write_label=True,
-                      line_color=(255, 255, 255),
-                      text_bg_color=(255, 255, 255),
-                      text_color=(0, 0, 0)):
+                         label,
+                         bbox,
+                         write_label=True,
+                         line_color=(255, 255, 255),
+                         text_bg_color=(255, 255, 255),
+                         text_color=(0, 0, 0)):
     """draws a pole from the middle of the object that is to be labeled and adds the label to the flag
     
     Parameters
@@ -135,13 +169,14 @@ def draw_flag_with_label(img,
     # write label
 
     if write_label:
-        text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+        text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                     2)[0][0]
         label_bg = [
-            start_point[0], start_point[1], start_point[0] + text_width[0],
+            start_point[0], start_point[1], start_point[0] + text_width,
             start_point[1] + 30
         ]
         cv2.rectangle(img, (label_bg[0], label_bg[1]),
-                    (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
+                      (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
         cv2.putText(img, label, (start_point[0] + 5, start_point[1] - 5 + 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
 
