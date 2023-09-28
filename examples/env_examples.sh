ec5cd92fb410bb1a16440d5c31fe3d9699d7b269
sc0v1n0 <sc0v1n001@gmail.com>
2023-09-27 22:52:25 -0300

    build: rename folder demo to examples and create jupyter notebooks expand examples functions this package

---
+++ examples/env_examples.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements_example.txt
---
+++ examples/single_object.py
@@ -0,0 +1,21 @@
+import json
+
+import bbox_visualizer as bbv
+import cv2
+
+img = cv2.imread('../images/source_single.jpg')
+annotation = json.load(open('../images/source_single.json'))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+points = annotation['shapes'][0]['points']
+label = annotation['shapes'][0]['label']
+(xmin, ymin), (xmax, ymax) = points
+bbox = [xmin, ymin, xmax, ymax]
+
+img_with_box = bbv.draw_rectangle(img, bbox)
+img_with_box_2 = img_with_box.copy()
+
+img_label = bbv.add_label(img_with_box, label, bbox)
+img_T_label = bbv.add_T_label(img_with_box_2, label, bbox)
+
+img_flag = bbv.draw_flag_with_label(img, label, bbox)
