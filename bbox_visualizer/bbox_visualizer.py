5ef882cd0b959bf51fa7ca2e19f93ff42fff7562
shreyasvedpathak <68227226+shreyasvedpathak@users.noreply.github.com>
2025-03-03 06:34:30 +0530

    Labelling issue and Bounding box drawing issue. (#8)
    * Fallback to normal labelling if out of frame
    
    * Added test images and test.py files
    
    * Handle bounding boxes on the edge properly
    
    * Made requested changes according to PR
    
    ---------
    
    Co-authored-by: Shoumik Chowdhury <shoumikchow@gmail.com>

--- bbox_visualizer/bbox_visualizer.py
+++ bbox_visualizer/bbox_visualizer.py
@@ -1,14 +1,46 @@
 import cv2
+import logging
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-def draw_rectangle(img,
-                   bbox,
-                   bbox_color=(255, 255, 255),
-                   thickness=3,
-                   is_opaque=False,
-                   alpha=0.5):
+def check_and_modify_bbox(bbox, img_size, margin=0):
+    """
+    Checks if the bounding box is inside the given image.
+    If not, the coordinates are trimmed inorder to fit inside the image.
+
+    Trimming criteria:
+        - for xmin and ymin:
+            - xmin and ymin are trimmed to 0, if their value is negative i.e. out of the
+            scope of image.
+        - for xmax and ymax:
+            - xmax and ymax are trimmed to image_width and image_height respectivel
+
+
+    Parameters
+    ----------
+    bbox : list
+        a list containing x_min, y_min, x_max and y_max of the rectangle positions
+    img_size: tuple
+        a tuple containing image width, image height and color channel.
+    margin: integer (default = 0)
+        space between edge of the image and bounding box incase the box is trimmed such that
+        it's size is exactly as the image.
+
+    Returns
+    -------
+    list
+        bounding box coordinates (xmin, ymin, xmax, ymax)
+    """
+    bbox = [value if value > 0 else margin for value in bbox]
+    bbox[2] = bbox[2] if bbox[2] < img_size[1] else img_size[1] - margin
+    bbox[3] = bbox[3] if bbox[3] < img_size[0] else img_size[0] - margin
+    return bbox
+
+
+def draw_rectangle(
+    img, bbox, bbox_color=(255, 255, 255), thickness=3, is_opaque=False, alpha=0.5
+):
     """Draws the rectangle around the object
 
     Parameters
@@ -31,30 +63,37 @@ def draw_rectangle(img,
     ndarray
         the image with the bounding box drawn
     """
+    bbox = check_and_modify_bbox(bbox, img.shape)
+
     output = img.copy()
     if not is_opaque:
-        cv2.rectangle(output, (bbox[0], bbox[1]), (bbox[2], bbox[3]),
-                      bbox_color, thickness)
+        cv2.rectangle(
+            output, (bbox[0], bbox[1]), (bbox[2], bbox[3]), bbox_color, thickness
+        )
     else:
         overlay = img.copy()
 
-        cv2.rectangle(overlay, (bbox[0], bbox[1]), (bbox[2], bbox[3]),
-                      bbox_color, -1)
+        cv2.rectangle(overlay, (bbox[0], bbox[1]), (bbox[2], bbox[3]), bbox_color, -1)
         cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
 
     return output
 
 
-def add_label(img,
-              label,
-              bbox,
-              size=1,
-              thickness=2,
-              draw_bg=True,
-              text_bg_color=(255, 255, 255),
-              text_color=(0, 0, 0),
-              top=True):
-    """adds label, inside or outside the rectangle
+def add_label(
+    img,
+    label,
+    bbox,
+    size=1,
+    thickness=2,
+    draw_bg=True,
+    text_bg_color=(255, 255, 255),
+    text_color=(0, 0, 0),
+    top=True,
+):
+    """
+    adds label, inside or outside the rectangle.
+    if label cannot be drawn on outside of the box, it puts label inside the box.
+
 
     Parameters
     ----------
@@ -83,23 +122,24 @@ def add_label(img,
         the image with the label written
     """
 
-    (label_width, label_height), baseline = cv2.getTextSize(label, font, size, thickness)
 
-    if top:
-        label_bg = [bbox[0], bbox[1], bbox[0] + label_width, bbox[1] - label_height - (15 * size)]
+    (text_width, text_height), baseline = cv2.getTextSize(label, font, size, thickness)
+
+    if top and bbox[1] - text_height > text_height:
+        label_bg = [bbox[0], bbox[1], bbox[0] + text_width, bbox[1] - text_height - (15 * size)]
         if draw_bg:
             cv2.rectangle(img, (label_bg[0], label_bg[1]),
                           (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
-
+            
         cv2.putText(img, label, (bbox[0] + 5, bbox[1] - (15 * size)), font, size, text_color, thickness)
     else:
         label_bg = [bbox[0], bbox[1], bbox[0] + label_width, bbox[1] + label_height + (15 * size)]
         if draw_bg:
+
             cv2.rectangle(img, (label_bg[0], label_bg[1]),
                           (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
         cv2.putText(img, label, (bbox[0] + 5, bbox[1] + (16 * size) + (4 * thickness)), font, size, text_color, thickness)
     return img
-
 
 def add_T_label(img,
                 label,
@@ -109,6 +149,7 @@ def add_T_label(img,
                 draw_bg=True,
                 text_bg_color=(255, 255, 255),
                 text_color=(0, 0, 0)):
+
     """adds a T label to the rectangle, originating from the top of the rectangle
 
     Parameters
@@ -139,19 +180,28 @@ def add_T_label(img,
     (label_width, label_height), baseline = cv2.getTextSize(label, font, size, thickness)
     # draw vertical line
     x_center = (bbox[0] + bbox[2]) // 2
-    y_top = bbox[1] - 50
-    cv2.line(img, (x_center, bbox[1]), (x_center, y_top), text_bg_color, 3)
+    line_top = y_top = bbox[1] - 50
 
     # draw rectangle with label
     y_bottom = y_top
-    y_top = y_bottom - label_height - 5
-    x_left = x_center - (label_width // 2) - 5
-    x_right = x_center + (label_width // 2) + 5
+    y_top = y_bottom - text_height - 5
+
+    if y_top < 0:
+        logging.warning(
+            "Labelling style 'T' going out of frame. Falling back to normal labeling."
+        )
+        return add_label(img, label, bbox)
+
+    cv2.line(img, (x_center, bbox[1]), (x_center, line_top), text_bg_color, 3)
+    x_left = x_center - (text_width // 2) - 5
+    x_right = x_center + (text_width // 2) + 5
     if draw_bg:
         cv2.rectangle(img, (x_left, y_top - 30), (x_right, y_bottom),
                       text_bg_color, -1)
     cv2.putText(img, label, (x_left + 5, y_bottom - (8 * size)),
                 font, size, text_color, thickness)
+    )
+
 
     return img
 
@@ -166,6 +216,7 @@ def draw_flag_with_label(img,
                          text_bg_color=(255, 255, 255),
                          text_color=(0, 0, 0),
                          ):
+
     """draws a pole from the middle of the object that is to be labeled and adds the label to the flag
 
     Parameters
@@ -199,8 +250,14 @@ def draw_flag_with_label(img,
     (label_width, label_height), baseline = cv2.getTextSize(label, font, size, thickness)
 
     x_center = (bbox[0] + bbox[2]) // 2
-    y_bottom = int((bbox[1] * .75 + bbox[3] * .25))
+    y_bottom = int((bbox[1] * 0.75 + bbox[3] * 0.25))
     y_top = bbox[1] - (y_bottom - bbox[1])
+    if y_top < 0:
+        logging.warning(
+            "Labelling style 'Flag' going out of frame. Falling back to normal labeling."
+        )
+        img = draw_rectangle(img, bbox, bbox_color=line_color)
+        return add_label(img, label, bbox)
 
     start_point = (x_center, y_top)
     end_point = (x_center, y_bottom)
@@ -210,14 +267,28 @@ def draw_flag_with_label(img,
     # write label
 
     if write_label:
+
+        text_width = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0][0]
         label_bg = [
-            start_point[0], start_point[1], start_point[0] + label_width,
-            start_point[1] - label_height - (10 * size)
+            start_point[0],
+            start_point[1],
+            start_point[0] + text_width,
+            start_point[1] + 30,
         ]
-        cv2.rectangle(img, (label_bg[0], label_bg[1]),
-                      (label_bg[2] + 5, label_bg[3]), text_bg_color, -1)
-        cv2.putText(img, label, (start_point[0] + 7, start_point[1] - (13 * size) + 20),
-                    font, size, text_color, thickness)
+        cv2.rectangle(
+            img,
+            (label_bg[0], label_bg[1]),
+            (label_bg[2] + 5, label_bg[3]),
+            text_bg_color,
+            -1,
+        )
+        cv2.putText(
+            img,
+            label,
+            (start_point[0] + 5, start_point[1] - 5 + 30),
+            font, size, text_color, thickness
+        )
+
     return img
 
 
@@ -226,12 +297,9 @@ def draw_flag_with_label(img,
 # INSTEAD OF THE FUNCTIONS BELOW
 
 
-def draw_multiple_rectangles(img,
-                             bboxes,
-                             bbox_color=(255, 255, 255),
-                             thickness=3,
-                             is_opaque=False,
-                             alpha=0.5):
+def draw_multiple_rectangles(
+    img, bboxes, bbox_color=(255, 255, 255), thickness=3, is_opaque=False, alpha=0.5
+):
     """draws multiple rectangles
 
     img : ndarray
@@ -252,10 +320,8 @@ def draw_multiple_rectangles(img,
     ndarray
         the image with the bounding boxes drawn
     """
-
     for bbox in bboxes:
-        img = draw_rectangle(img, bbox, bbox_color, thickness, is_opaque,
-                             alpha)
+        img = draw_rectangle(img, bbox, bbox_color, thickness, is_opaque, alpha)
     return img
 
 
@@ -268,6 +334,7 @@ def add_multiple_labels(img,
                         text_bg_color=(255, 255, 255),
                         text_color=(0, 0, 0),
                         top=True):
+
     """add labels, inside or outside the rectangles
 
     Parameters
@@ -295,16 +362,17 @@ def add_multiple_labels(img,
     for label, bbox in zip(labels, bboxes):
         img = add_label(img, label, bbox, size, thickness, draw_bg, text_bg_color, text_color,
                         top)
-
     return img
 
 
-def add_multiple_T_labels(img,
-                          labels,
-                          bboxes,
-                          draw_bg=True,
-                          text_bg_color=(255, 255, 255),
-                          text_color=(0, 0, 0)):
+def add_multiple_T_labels(
+    img,
+    labels,
+    bboxes,
+    draw_bg=True,
+    text_bg_color=(255, 255, 255),
+    text_color=(0, 0, 0),
+):
     """adds T labels to the rectangles, each originating from the top of the rectangle
 
     Parameters
@@ -336,13 +404,15 @@ def add_multiple_T_labels(img,
     return img
 
 
-def draw_multiple_flags_with_labels(img,
-                                    labels,
-                                    bboxes,
-                                    write_label=True,
-                                    line_color=(255, 255, 255),
-                                    text_bg_color=(255, 255, 255),
-                                    text_color=(0, 0, 0)):
+def draw_multiple_flags_with_labels(
+    img,
+    labels,
+    bboxes,
+    write_label=True,
+    line_color=(255, 255, 255),
+    text_bg_color=(255, 255, 255),
+    text_color=(0, 0, 0),
+):
     """draws poles from the middle of the objects that are to be labeled and adds the labels to the flags
 
     Parameters
@@ -369,6 +439,7 @@ def draw_multiple_flags_with_labels(img,
     """
 
     for label, bbox in zip(labels, bboxes):
+
         img = draw_flag_with_label(img, label, bbox, size=1, thickness=2,
                                 write_label=write_label, line_color=line_color,
                                 text_bg_color=text_bg_color,
---
+++ tests/test_single_object.py
@@ -0,0 +1,28 @@
+import json
+
+import bbox_visualizer as bbv
+import cv2
+
+img = cv2.imread('../images/test_images/source_bird.jpg')
+annotation = json.load(open('../images/test_images/source_bird.json'))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+points = annotation['shapes'][0]['points']
+label = annotation['shapes'][0]['label']
+(xmin, ymin), (xmax, ymax) = points
+bbox = [xmin, ymin, xmax, ymax]
+
+img_with_box = bbv.draw_rectangle(img, bbox)
+img_with_box_2 = img_with_box.copy()
+
+img_label = bbv.add_label(img_with_box, label, bbox)
+img_T_label = bbv.add_T_label(img_with_box_2, label, bbox)
+
+img_flag = bbv.draw_flag_with_label(img, label, bbox)
+
+cv2.imshow("img_with_box", img_with_box)
+cv2.imshow("img_with_T_label", img_T_label)
+cv2.imshow("img_with_flag", img_flag)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
